8ce53239293bc58bd20645ff52423f7d8809bc8f
noisenuisance <sabauspne@gmail.com>
2022-10-13 23:06:08 +0300

    feat: set favicon

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import "./SearchForm.css";
 
 const SearchForm = ({ isLoading }) => {
+  const [isInputEmpty, setIsInputEmpty] = useState(true);
+  const handleIsEmpty = (evt) => {
+    setIsInputEmpty(evt.target.value.length === 0);
+  };
+  const clearSearch = (evt) => {
+    evt.target.closest(".search-form").reset();
+    setIsInputEmpty(true);
+  };
   return (
     <>
       <form className="search-form" name="search-movie">
         <div className="search-form__wrapper">
-          <div className="search-form__icon" />
-          <input className="search-input" name="movie" placeholder="Фильм" />
+          <button
+            type="button"
+            className={`search-form__reset-btn ${
+              !isInputEmpty && "search-form__reset-btn_active"
+            }`}
+            disabled={isInputEmpty}
+            onClick={!isInputEmpty ? clearSearch : undefined}
+          />
+          <input
+            className="search-input"
+            name="movie"
+            placeholder="Фильм"
+            onChange={handleIsEmpty}
+          />
           <button
             type="submit"
             className={`search-btn ${isLoading ? "search-btn_loading" : ""}`}
